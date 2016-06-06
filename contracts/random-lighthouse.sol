36a2c6587131be33385f9252a5be0afb5cf224f1
Victory <git@dfhu.org>
2016-06-06 19:18:25 -0400

    Random

---
+++ contracts/random-lighthouse.sol
@@ -0,0 +1,52 @@
+contract abstract {}
+
+contract owned is abstract {
+  address owner;
+  function owned () {
+    owner = msg.sender;
+ }
+  function changeOwner (address newOwner) forOwner {
+    owner = newOwner;
+  }
+  modifier forOwner (){
+    if (msg.sender==owner) _
+  }
+}
+
+contract RandomLighthouse is owned {
+  uint32 uniformPositive;
+  uint32 curTic;
+  address owner;
+  uint balance;
+
+  address[] waitingUniformPositiveInt;
+
+
+  function debug (bytes32 msg) {
+    log0(msg);
+  }
+
+  function RandomLighthouse () {
+    curTic = 0;
+    owned;
+  }
+
+  function subscribeToNextUniformPositiveInt () returns (uint32) {
+    waitingUniformPositiveInt.push(msg.sender);
+  }
+
+  function getUniformPositiveInt () returns(uint32) {
+    return uniformPositive;
+  }
+
+  function setUniformPositiveInt (uint32 val) forOwner {
+    uniformPositive = val;
+    curTic += 1;
+  }
+
+  function withdrawl() {
+    if (msg.sender != owner) throw;
+    //log0(balance);
+    owner.send(balance/2);
+  }
+}
