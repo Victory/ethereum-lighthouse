b068360861a3ac83bd197c89e46cbe1d820bd3e2
Victory <git@dfhu.org>
2016-06-23 18:59:52 -0400

    Request balance, use fromWei, document installing solc.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # ethereum-lighthouse
 Pushed Services for Ethereum
+
+## To install:
+
+## solc
+
+From offical documents
+
+http://www.ethdocs.org/en/latest/ethereum-clients/cpp-ethereum/installing-binaries/linux-ubuntu-ppa.html
+
+```
+sudo add-apt-repository ppa:ethereum/ethereum-qt
+sudo add-apt-repository ppa:ethereum/ethereum
+sudo apt-get update
+sudo apt-get install cpp-ethereum
+
+```
+
--- html/js/home.js
+++ html/js/home.js
@@ -1,7 +1,24 @@
+var web3 = new Web3(new Web3.providers.HttpProvider("/parity"));
+var eth = web3.eth;
 jQuery(function ($) {
-  var web3 = new Web3(new Web3.providers.HttpProvider("/parity"));
+  var coin;
+
+  var showBalance = function () {
+    var bal = eth.getBalance(coin);
+    $("#balanceHeader").text("Current balance is: ");
+    $("#balance").text(web3.fromWei(bal, 'ether'));
+  };
+
+  $("#setCoin").click(function () {
+    coin = $("#coin").val();
+    showBalance();
+  });
+
+  /*
   var filter = web3.eth.filter('latest');
   filter.watch(function (err, result) {
     //console.log(result, err)
   });
+  */
+  web3.eth.getBalance("")
 });
