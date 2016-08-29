2c999ec6ee0d2bd1b4ac4e463bf9ffdbe0128e31
Victory <git@dfhu.org>
2016-08-29 13:35:15 -0500

    Add whole source code of ping to pong sol

--- contracts/ping.sol
+++ contracts/ping.sol
@@ -1,5 +1,13 @@
 contract Pong {
+  int32 curNum = 0;
+
+  event SendingPong(int32);
+
   function getVal() public returns (int32) {
+    curNum = curNum + 1;
+
+    SendingPong(curNum);
+    return curNum;
   }
 }
 
