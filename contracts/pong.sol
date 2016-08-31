116333fa2a27d6ea5a9558489b8e5f5b288088fd
Victory <git@dfhu.org>
2016-08-31 17:54:43 -0500

    More events in pong

--- contracts/pong.sol
+++ contracts/pong.sol
@@ -1,8 +1,11 @@
 contract Pong {
   int32 curNum = 0;
+  int32 pushedVal;
 
   event SendingPong(int32);
   event SendingClock(string);
+  event ValPushed(int32);
+  event ValPulled(int32);
 
   function getVal() public returns (int32) {
     curNum = curNum + 1;
@@ -19,5 +22,15 @@ contract Pong {
       SendingClock("NOT mod 2");
       return "NOT mod 2";
     }
+  }
+
+  function pushVal(int32 val) {
+    pushedVal = val;
+    ValPushed(val);
+  }
+
+  function pullVal() public returns (int32) {
+    ValPulled(pushedVal);
+    return pushedVal;
   }
 }
